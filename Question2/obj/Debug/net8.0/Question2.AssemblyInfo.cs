0665d652ef79f69d2bd56fd428f4ca1e76ff0a39
Khuzaim Adnan Qureshi <khuzaimadnana@gmail.com>
2025-01-08 15:50:50 +0500

    new

--- Question2/obj/Debug/net8.0/Question2.AssemblyInfo.cs
+++ Question2/obj/Debug/net8.0/Question2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Question2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1417862d632cb965afc9e7f04b757d0459b7fb24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Question2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Question2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
